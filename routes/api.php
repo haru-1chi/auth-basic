8973c5c795770b44d859978143129a40e08dac5b
haru-1chi <jarunee.recc2@gmail.com>
2024-01-19 13:07:12 +0700

    install JWT

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::group([
+    'prefix' => 'auth',
+    'middleware' => 'api',
+], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('resetPassword', [AuthController::class, 'resetPassword']);
+    Route::post('password/forgot', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.forgot');;
+    Route::post('password/reset', [ForgotPasswordController::class, 'reset'])->name('password.reset');;
+    Route::get('me', [AuthController::class, 'me']);
 });
